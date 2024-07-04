467c0d8d7fed33b4173bd8af651bcf53f7238bcf
elyse502 <elyseniyibizi502@gmail.com>
2024-07-04 11:16:55 +0300

    Use Insomnia - Postman - REST clients for making HTTP requests very easily 🌐

--- src/server.js
+++ src/server.js
@@ -3,7 +3,8 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/hello', (req, res) => {
+app.post('/add', (req, res) => {
+    res.status(201); // CREATED
     res.send('Hello World!');
 });
 
