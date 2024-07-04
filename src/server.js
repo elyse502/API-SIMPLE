4f19f4abc046b04c58d39bbc628b7340c02ea987
elyse502 <elyseniyibizi502@gmail.com>
2024-07-04 12:33:07 +0300

    Start Building API Endpoints 🏗️

--- src/server.js
+++ src/server.js
@@ -4,9 +4,33 @@ import { StatusCodes } from 'http-status-codes';
 const app = express();
 const port = 3000;
 
+const STATUS = {
+    SUCCESS: 'OK',
+    FAILURE: 'NO'
+};
+
+app.use(express.json());
+
 app.get('/hello-world', (req, res) => {
     res.status(StatusCodes.OK);
     res.send('Hello World!');
+});
+
+app.post('/add', (req, res) => {
+    const data = [];
+    const { body } = req;
+
+    if (!body.name) {
+        return res.status(StatusCodes.BAD_REQUEST).send({
+            status: STATUS.FAILURE,
+            message: 'Name is required'
+        });
+    }
+
+    return res.status(StatusCodes.CREATED).send({
+        status: STATUS.SUCCESS,
+        message: data
+    });
 });
 
 app.listen(port, () => {
