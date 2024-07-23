f9de418a0815673c0bb9ea6a84b20e0e58decf7a
elyse502 <elyseniyibizi502@gmail.com>
2024-07-23 12:11:37 +0300

    Validate our endpoints pt2 🖇️

--- src/user.schemas.js
+++ src/user.schemas.js
@@ -1,5 +1,18 @@
 import * as yup from 'yup';
 
+const MIN_LENGTH = {
+    name: 2,
+    city: 1,
+    country: 2
+};
+
+const MAX_LENGTH = {
+    name: 20,
+    city: 30,
+    country: 30,
+    email: 100
+};
+
 export const getUser = {
     schema: {
         params: {
@@ -14,10 +27,10 @@ export const addUser = {
     schema: {
         body: {
             yupSchema: yup.object().shape({
-                name: yup.string(),
-                email: yup.string().email(),
-                city: yup.string(),
-                country: yup.string(),
+                name: yup.string().min(MIN_LENGTH.name).max(MAX_LENGTH.name),
+                email: yup.string().email().max(MAX_LENGTH.email),
+                city: yup.string().min(MIN_LENGTH.city).max(MAX_LENGTH.city),
+                country: yup.string().min(MIN_LENGTH.country).max(MAX_LENGTH.country),
             })
         },
     },
@@ -25,12 +38,17 @@ export const addUser = {
 
 export const updateUser = {
     schema: {
+        params: {
+            yupSchema: yup.object().shape({
+                id: yup.number().required(),
+            })
+        },
         body: {
             yupSchema: yup.object().shape({
-                name: yup.string(),
+                name: yup.string().min(MIN_LENGTH.name).max(MAX_LENGTH.name),
                 email: yup.string().email(),
-                city: yup.string(),
-                country: yup.string(),
+                city: yup.string().min(MIN_LENGTH.city).max(MAX_LENGTH.city),
+                country: yup.string().min(MIN_LENGTH.country).max(MAX_LENGTH.country),
             })
         },
     },
@@ -38,7 +56,7 @@ export const updateUser = {
 
 export const removeUser = {
     schema: {
-        body: {
+        params: {
             yupSchema: yup.object().shape({
                 id: yup.number().required(),
             })
