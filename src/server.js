8bc0fb854ebcd39508d5d945f7e3eef024f15afa
elyse502 <elyseniyibizi502@gmail.com>
2024-07-23 12:22:53 +0300

    Secure our API - Potential Vulnerabilities 🔐

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import helmet from 'helmet';
 
 import mainRoutes from './main.routes';
 import userRoutes from './user.routes';
@@ -7,6 +8,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(helmet());
 
 app.use('/v1', mainRoutes);
 app.use('/v1/user', userRoutes);
